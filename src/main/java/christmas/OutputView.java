16513f8a0f587cb104953117fc0cbf8311d9f2a1
Taehyoun Kim <kth9903@gmail.com>
2023-11-13 20:26:50 +0900

    feat(outputview) : 출력 클래스 추가

---
+++ src/main/java/christmas/OutputView.java
@@ -0,0 +1,11 @@
+package christmas;
+
+public class OutputView {
+    public void printMenu() {
+        System.out.println("<주문 메뉴>");
+        // ...
+    }
+    public static void printDiscounts(Discounts discounts) {
+        // ...
+    }
+}
