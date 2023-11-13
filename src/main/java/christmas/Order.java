1bed2bfd40f8bcb8e4c3644d05c6f66a6212c7c6
Taehyoun Kim <kth9903@gmail.com>
2023-11-13 22:02:21 +0900

    feat(order) : 주문 품목 클래스 추가

---
+++ src/main/java/christmas/Order.java
@@ -0,0 +1,29 @@
+package christmas;
+
+class Order {
+    private String itemName;
+    private int quantity;
+    private int price;
+
+    public Order(String itemName, int quantity, int price){
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getItemName(){
+        return itemName;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public int getTotalPrice(){
+        return price * quantity;
+    }
+}
