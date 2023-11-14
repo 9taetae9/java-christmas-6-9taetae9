1d310c489b56d323068a8547763b2845bc617079
Taehyoun Kim <kth9903@gmail.com>
2023-11-15 01:04:00 +0900

    feat(outputview) : 인사, 주문 메뉴, 할인 내역 출력 메서드 구현

--- src/main/java/christmas/OutputView.java
+++ src/main/java/christmas/OutputView.java
@@ -1,11 +1,41 @@
 package christmas;
 
+import javax.swing.plaf.basic.BasicDesktopIconUI;
+import java.util.List;
+
 public class OutputView {
-    public void printMenu() {
+    public static void greeting(){
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플레너입니다.");
+    }
+    public static void printMenu(List<Order> orders){
         System.out.println("<주문 메뉴>");
-        // ...
+        for(Order order : orders){
+            System.out.println(order.getItemName() + " " + order.getQuantity() + "개");
+        }
     }
-    public static void printDiscounts(Discounts discounts) {
-        // ...
+    private static String getDiscountDetailsMessage(int totalDiscount) {
+        if (totalDiscount > 0) {
+            return "크리스마스 디데이 할인: -" + totalDiscount + "원";
+        } else {
+            return "없음";
+        }
     }
+
+    private static String getBadgeMessage(Badge badge) {
+        if (badge != Badge.NONE) {
+            return badge.getBadgeName();
+        } else {
+            return "없음";
+        }
+    }
+
+    public static void printOrderDetails(int totalOrderAmount, int totalDiscount, Badge badge) {
+        System.out.println("<할인 전 총주문 금액>\n" + totalOrderAmount + "원");
+        System.out.println("<증정 메뉴>\n" + GiftProvider.getChampagneMessage(totalOrderAmount));
+        System.out.println("<혜택 내역>\n" + getDiscountDetailsMessage(totalDiscount));
+        System.out.println("<총혜택 금액>\n-" + totalDiscount + "원");
+        System.out.println("<할인 후 예상 결제 금액>\n" + (totalOrderAmount - totalDiscount) + "원");
+        System.out.println("<12월 이벤트 배지>\n" + getBadgeMessage(badge));
+    }
+
 }
