ab6734269a78269adf167bbc3c2eebeef70a4487
Taehyoun Kim <kth9903@gmail.com>
2023-11-15 22:03:38 +0900

    feat(application) : 크리스마스 프로모션 프로그램 메인 로직 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,15 @@
 package christmas;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView.greeting();
+
+        int visitDate = InputView.readDate();
+        List<Order> orders = InputView.readOrder();
+
+        OutputView.printMenu(orders);
+        OutputView.printOrderDetails(visitDate, orders);
     }
 }
