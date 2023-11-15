ebde1d50de8345266e5d3c191273bb0fb3c2baa2
Taehyoun Kim <kth9903@gmail.com>
2023-11-15 22:01:26 +0900

    refactor(outputview) : 혜택_내역_없음_출력 테스트 케이스를 통과하기 위해 출력 방식 수정

--- src/main/java/christmas/OutputView.java
+++ src/main/java/christmas/OutputView.java
@@ -2,6 +2,8 @@ package christmas;
 
 import java.util.List;
 
+import static christmas.DiscountCalculator.calculateTotalDiscountWithoutChampagne;
+
 public class OutputView {
     public static void greeting(){
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플레너입니다.");
@@ -12,6 +14,7 @@ public class OutputView {
         for(Order order : orders){
             System.out.println(order.getMenuName() + " " + order.getMenuQuantity() + "개");
         }
+        System.out.println();
     }
     private static String getDiscountDetailsMessage(int totalDiscount) {
         if (totalDiscount > 0) {
@@ -21,13 +24,31 @@ public class OutputView {
     }
 
 
-    public static void printOrderDetails(int totalOrderAmount, int totalDiscount) {
-        System.out.println("<할인 전 총주문 금액>\n" + totalOrderAmount + "원");
-        System.out.println("<증정 메뉴>\n" + GiftProvider.getChampagneMessage(totalOrderAmount));
-        System.out.println("<혜택 내역>\n" + getDiscountDetailsMessage(totalDiscount));
-        System.out.println("<총혜택 금액>\n-" + totalDiscount + "원");
-        System.out.println("<할인 후 예상 결제 금액>\n" + DiscountCalculator.calculateFinalPayment(totalOrderAmount, totalDiscount) + "원");
-        System.out.println("<12월 이벤트 배지>\n" + Badge.getBadgeByDiscount(totalDiscount));
+    public static void printOrderDetails(int date, List<Order> orders) {
+        int totalOrderAmount = DiscountCalculator.totalOrderAmount(orders);
+        int totalDiscountExcludingChampagne = calculateTotalDiscountWithoutChampagne(date, orders);
+        int finalPayment = totalOrderAmount - totalDiscountExcludingChampagne;
+        int totalDiscountIncludingChampagne = calculateTotalDiscountIncludingChampagne(date, orders);
+        Badge badge = Badge.getBadgeByDiscount(totalDiscountIncludingChampagne);
+
+        String discountDetails = DiscountCalculator.constructDiscountDetails(date, orders);
+
+        System.out.println("<할인 전 총주문 금액>\n" + formatCurrency(totalOrderAmount)+'\n');
+        System.out.println("<증정 메뉴>\n" + (DiscountCalculator.qualifiesForChampagne(totalOrderAmount) ? "샴페인 1개\n" : "없음\n"));
+        System.out.println("<혜택 내역>");
+        System.out.println(discountDetails+'\n');
+        System.out.println("<총혜택 금액>\n" + formatCurrency(-totalDiscountIncludingChampagne)+'\n');
+        System.out.println("<할인 후 예상 결제 금액>\n" + formatCurrency(finalPayment)+'\n');
+        System.out.println("<12월 이벤트 배지>\n" + badge.getBadgeName());
+    }
+
+    private static int calculateTotalDiscountIncludingChampagne(int date, List<Order> orders) {
+        return calculateTotalDiscountWithoutChampagne(date, orders) +
+                (DiscountCalculator.qualifiesForChampagne(DiscountCalculator.totalOrderAmount(orders)) ? Discount.CHAMPAGNE_VALUE.getPrice() : 0);
+    }
+
+    private static String formatCurrency(int amount) {
+        return String.format("%,d원", amount);
     }
 
 }
