8c9e1d60b36ac148297f2fb8ff15b12cd5e004aa
Taehyoun Kim <kth9903@gmail.com>
2023-11-15 15:40:19 +0900

    feat(inputview) : 사용자가 잘못 입력한 부분부터 재입력 요구 기능 추가

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -1,31 +1,43 @@
 package christmas;
 
 import camp.nextstep.edu.missionutils.Console;
-import org.mockito.internal.matchers.Or;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
     public static int readDate() {
-        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String date = Console.readLine();
-        Eventvalidator.isDateValid(Integer.parseInt(date));
-        return Integer.parseInt(date);
+        while(true) {
+            System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+            try{
+                int date = Integer.parseInt(Console.readLine().trim());
+                EventValidator.isDateValid(date);
+                return date;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
     }
+
     public static List<Order> readOrder() {
         List<Order> orders = new ArrayList<>();
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        String order = Console.readLine();
-        Eventvalidator.isOrderValid(order);
-        String[] ordersInputs = order.split(",");
-        for(String orderInputs : ordersInputs){
-            String[] parts = orderInputs.trim().split("-");
-            String itemName = parts[0].trim();
-            int quantity = Integer.parseInt(parts[1].trim());
-            orders.add(new Order(itemName, quantity));
+        while(true) {
+            try {
+                String[] userOrders = Console.readLine().trim().split(",");
+                for (String order : userOrders) {
+                    EventValidator.validateOrderInput(order);
+                    String[] orderParts = order.trim().split("-");
+                    String menuName = orderParts[0].trim();
+                    int menuQuantity = Integer.parseInt(orderParts[1].trim());
+                    orders.add(new Order(menuName, menuQuantity));
+                }
+                return orders;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+                orders.clear();
+            }
         }
-        return orders;
     }
 
 }
