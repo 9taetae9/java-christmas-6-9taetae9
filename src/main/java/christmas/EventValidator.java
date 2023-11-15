ba7a6b52e5ddc9706eb38b1c745f070283e3fb7a
Taehyoun Kim <kth9903@gmail.com>
2023-11-15 22:53:54 +0900

    refactor(eventvalidator) : 중복성을 줄이고 책임의 명확한 분리를 위해 사용자 입력 검증에 필요한 로직들을 다시 정리

--- src/main/java/christmas/EventValidator.java
+++ src/main/java/christmas/EventValidator.java
@@ -1,7 +1,6 @@
 package christmas;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class EventValidator {
     public static void isDateValid(int date) {
@@ -10,23 +9,51 @@ public class EventValidator {
         }
     }
 
-    public static void validateOrderInput(String orderInput) {
-        if (orderInput == null || orderInput.trim().isEmpty()) {
+    public static List<Order> validateAndParseOrders(String orderInputLine) {
+        if (orderInputLine == null || orderInputLine.trim().isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
-        String[] parts = orderInput.trim().split("-");
-        if (parts.length != 2) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-        }
-        String menuName = parts[0].trim();
-        if(!isMenuNameValid(menuName)){
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-        }
-        try {
-            int menuQuantity = Integer.parseInt(parts[1].trim());
-            if(!isQuantityValid(menuQuantity)){
+
+        String[] userOrders = orderInputLine.trim().split(",");
+        Set<String> uniqueMenuNames = new HashSet<>();
+        List<Order> orders = new ArrayList<>();
+
+        for (String userOrder : userOrders) {
+            String[] parts = userOrder.trim().split("-");
+            if (parts.length != 2) {
                 throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
             }
+
+            String menuName = parts[0].trim();
+            if (!uniqueMenuNames.add(menuName)) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+
+            if (!isMenuNameValid(menuName)) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+
+            int quantity = parseQuantity(parts[1].trim());
+            orders.add(new Order(menuName, quantity));
+        }
+
+        validateNotOnlyOrderedBeverage(orders);
+
+        return orders;
+    }
+
+    private static boolean isMenuNameValid(String menu) {
+        return Arrays.stream(Menu.values())
+                .anyMatch(m -> m.getMenuname().equalsIgnoreCase(menu));
+    }
+
+    private static int parseQuantity(String quantityStr) {
+        try {
+            int quantity = Integer.parseInt(quantityStr);
+            if (quantity < 1) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+            return quantity;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
@@ -36,16 +63,7 @@ public class EventValidator {
         boolean nonBeverageFound = orders.stream()
                 .anyMatch(order -> !order.getMenuCategory().equalsIgnoreCase("Beverage"));
         if (!nonBeverageFound) {
-            throw new IllegalArgumentException("[ERROR] 주문에는 음료만 포함될 수 없습니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
-    }
-
-    private static boolean isMenuNameValid(String menu) {
-        return Arrays.stream(Menu.values())
-                .anyMatch(m -> Boolean.parseBoolean(m.getMenuname()));
-    }
-
-    private static boolean isQuantityValid(int quantity) {
-        return quantity >= 1;
     }
 }
