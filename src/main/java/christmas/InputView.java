e1776f754a98a4d7cd30507d1822fd829aca25fb
Taehyoun Kim <kth9903@gmail.com>
2023-11-15 22:52:01 +0900

    refactor(inputview) : 클래스 역할에 맞지 않는 검증 메서드들을 삭제

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -2,10 +2,7 @@ package christmas;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class InputView {
     public static int readDate() {
@@ -31,45 +28,12 @@ public class InputView {
     public static List<Order> readOrder() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         while (true) {
-            String orderInputLine = Console.readLine();
             try {
-                return parseOrders(orderInputLine);
+                String orderInputLine = Console.readLine();
+                return EventValidator.validateAndParseOrders(orderInputLine);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
-
-    private static List<Order> parseOrders(String orderInputLine) throws IllegalArgumentException {
-        String[] userOrders = orderInputLine.trim().split(",");
-        List<Order> orders = new ArrayList<>();
-        Set<String> orderNames = new HashSet<>();
-
-        for (String userOrder : userOrders) {
-            String[] orderParts = userOrder.trim().split("-");
-            if (orderParts.length != 2) {
-                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-            }
-
-            String menuName = orderParts[0].trim();
-            int menuQuantity = parseQuantity(orderParts[1].trim());
-
-            if (!orderNames.add(menuName)) {
-                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-            }
-
-            orders.add(new Order(menuName, menuQuantity));
-        }
-        EventValidator.validateNotOnlyOrderedBeverage(orders);
-        return orders;
-    }
-
-    private static int parseQuantity(String quantityStr) {
-        try {
-            return Integer.parseInt(quantityStr);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-        }
-    }
-
 }
