ad712275a22864a88496dd800d79fa3c152dc325
Taehyoun Kim <kth9903@gmail.com>
2023-11-15 21:55:00 +0900

    refactor(discountcalculator) : 새로운 매서드 구현으로 인해 미사용된 매서드 및 변수 제거

--- src/main/java/christmas/DiscountCalculator.java
+++ src/main/java/christmas/DiscountCalculator.java
@@ -5,8 +5,6 @@ import java.util.Set;
 
 public class DiscountCalculator {
    public static int calculateTotalDiscount(int date, List<Order> orders) {
-      boolean isWeekend = isWeekend(date);
-      boolean isSpecialDay = isSpecialDay(date);
       int totalDiscount = 0;
 
       // 크리스마스 디데이 할인 계산
@@ -21,17 +19,12 @@ public class DiscountCalculator {
       return totalDiscount;
    }
 
-   public static int calculateFinalPayment(int totalOrderAmount, int totalDiscountWithoutChampagne) {
-      return totalOrderAmount - totalDiscountWithoutChampagne;
-   }
-
    public static int calculateTotalDiscountWithoutChampagne(int date, List<Order> orders) {
       int totalDiscount = 0;
       for (Order order : orders) {
          totalDiscount += calculateOrderDiscount(date, order);
       }
       totalDiscount += calculateChristmasDiscount(date);
-      // Special discount should be added only once
       if (isSpecialDay(date)) {
          totalDiscount += Discount.SPECIAL_DISCOUNT.getPrice();
       }
@@ -57,17 +50,12 @@ public class DiscountCalculator {
       return discountPerItem * order.getMenuQuantity();
    }
 
-
-
-
    public static String constructDiscountDetails(int date, List<Order> orders) {
       StringBuilder discountDetails = new StringBuilder();
-      int totalDiscount = 0;
 
       int christmasDiscount = calculateChristmasDiscount(date);
       if (christmasDiscount > 0) {
          addDiscountDetail(discountDetails, "크리스마스 디데이 할인", christmasDiscount);
-         totalDiscount += christmasDiscount;
       }
 
       int weekdayDiscountTotal = 0;
@@ -83,21 +71,16 @@ public class DiscountCalculator {
 
       if (weekdayDiscountTotal > 0) {
          addDiscountDetail(discountDetails, "평일 할인", weekdayDiscountTotal);
-         totalDiscount += weekdayDiscountTotal;
       }
       if (weekendDiscountTotal > 0) {
          addDiscountDetail(discountDetails, "주말 할인", weekendDiscountTotal);
-         totalDiscount += weekendDiscountTotal;
       }
 
-      // Special discount applies only once regardless of the number of items
       if (isSpecialDay(date)) {
          int specialDiscount = Discount.SPECIAL_DISCOUNT.getPrice();
          addDiscountDetail(discountDetails, "특별 할인", specialDiscount);
-         totalDiscount += specialDiscount;
       }
 
-      // Champagne discount is shown in details but not added to totalDiscount
       if (qualifiesForChampagne(totalOrderAmount(orders))) {
          addDiscountDetail(discountDetails, "증정 이벤트", Discount.CHAMPAGNE_VALUE.getPrice());
       }
@@ -105,33 +88,18 @@ public class DiscountCalculator {
       return discountDetails.length() > 0 ? discountDetails.toString().trim() : "없음";
    }
 
-   public static int getTotalDiscountIncludingChampagne(int date, List<Order> orders) {
-      return calculateTotalDiscount(date, orders) +
-              (qualifiesForChampagne(totalOrderAmount(orders)) ? Discount.CHAMPAGNE_VALUE.getPrice() : 0);
-   }
-
    private static void addDiscountDetail(StringBuilder details, String type, int amount) {
-      details.append(type).append(": -").append(amount).append("원\n");
-   }
-
-   private static String getDiscountType(String category, int date) {
-      if ("Main".equals(category) && isWeekend(date)) {
-         return "주말 할인";
-      } else if ("Dessert".equals(category) && !isWeekend(date)) {
-         return "평일 할인";
+      if (amount != 0) {
+         String formattedAmount = String.format("%,d원", amount);
+         details.append(type).append(": -").append(formattedAmount).append("\n");
+      } else {
+         details.append(type).append(":\n없음\n");
       }
-      return "";
    }
 
 
    public static int totalOrderAmount(List<Order> orders) {
       return orders.stream().mapToInt(Order::getTotalPrice).sum();
-   }
-
-   private static int champagneDiscount(int totalOrderAmount) {
-      if(totalOrderAmount >= Discount.CHAMPAGNE_GIFT_THRESHOLD.getPrice()){
-         return Discount.CHAMPAGNE_VALUE.getPrice();
-      } return 0;
    }
 
    public static boolean qualifiesForChampagne(int totalOrderAmount) {
